6cfac0488909eee5e93bd789a0cd013395038d73
Ansh D Mishra <anshvnm@gmail.com>
2022-10-19 00:15:35 +0530

    Initial Commit
    Check whether word, number, phrase, or a string is a palindrome or not

---
+++ pc.js
@@ -0,0 +1,18 @@
+// This is a function that shows var omega getting the value from the input element
+function palindrome(){
+    var num = document.querySelector(".inputbox").value.toUpperCase();
+//   The code that organises and checks if it is a palindrome
+    var alpha = num
+        .split('')
+        .reverse()
+        .join('');
+// This is an if statement for when the letters are less than or equal to 2 and an else if statement for when the letters are greater than or equal to 3 and whether or not omega is strictly equal to alpha
+    if(num.length<=2){
+        document.querySelector(".list").innerHTML = "TYPE MORE VALUES"
+    }else if(num.length>=3 && num===alpha){
+        document.querySelector(".list").innerHTML =`<b>"${num}"</b> is a palindrome!`
+    }else{
+        document.querySelector(".list").innerHTML =`<b>"${num}"</b> is not a palindrome!`
+    }
+}
+
